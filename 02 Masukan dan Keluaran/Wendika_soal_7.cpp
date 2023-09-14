ec2f0dcb53bd2c32176a3830a378b98d3e8fd8a3
wenkzofficial <wendikaismail101@gmail.com>
2023-09-14 14:04:48 +0700

    Tugas 2 DDP

--- 02 Masukan dan Keluaran/Wendika_soal_7.cpp
+++ 02 Masukan dan Keluaran/Wendika_soal_7.cpp
@@ -3,14 +3,15 @@ using namespace std;
 
 int main() {
     string names[12];
-    for (int i = 0; i < 12; i++) {
-        cin >> names[i];
-    }
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 4; j++) {
-            cout << names[i * 4 + j] << " ";
-        }
-        cout << endl;
-    }
+    
+    cin >> names[0] >> names[1] >> names [2];
+    cin >> names[3] >> names[4] >> names [5];
+    cin >> names[6] >> names[7] >> names [8];
+    cin >> names[9] >> names[10] >> names [11];
+    
+    cout << names[0] << " " << names[3] << " " << names[6] << " " << names[9] << endl;
+    cout << names[1] << " " << names[4] << " " << names[7] << " " << names[10] << endl;
+    cout << names[2] << " " << names[5] << " " << names[8] << " " << names[11] << endl;
+    
     return 0;
 }
