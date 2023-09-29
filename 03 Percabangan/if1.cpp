21d3207fb19ab137b06e5a8706aa729bb2d64030
wenkzofficial <wendikaismail101@gmail.com>
2023-09-29 08:44:29 +0700

    ifelse01

---
+++ 03 Percabangan/if1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cin >> a;
+    if (a == 50) {
+        cout << "SETENGAH" << endl;
+    } 
+    return 0;
+}
---
+++ 03 Percabangan/if2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cin >> a;
+    if (a == 100) {
+        cout << "SEMPURNA" << endl;
+    } else {
+        cout << "BELUM SEMPURNA" << endl;
+    }
+    return 0;
+}
