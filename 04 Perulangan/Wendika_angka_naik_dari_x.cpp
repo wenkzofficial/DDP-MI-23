372e30c3be64d1c94bc4e517096bf5855929278e
wenkzofficial <wendikaismail101@gmail.com>
2023-10-12 21:38:00 +0700

    TugasPerulangan

---
+++ 04 Perulangan/Wendika_angka_naik_dari_x.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+
+    for (int i = x; i <= y; i++) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
